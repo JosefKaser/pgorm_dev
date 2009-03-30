fddfb6e227444467c60811d72f5248eb39fa9074
gevikb@gmail.com <gevikb@gmail.com@96a1c612-1d74-11de-a108-cd2f117ce590>
2009-03-30 22:01:02 +0000

    minor update for code readability 2

--- trunk/PGORMWizard/Helper.cs
+++ trunk/PGORMWizard/Helper.cs
@@ -6,12 +6,14 @@ using System.Drawing;
 
 namespace PGORMWizard
 {
+    #region ParameterName
     internal class ParameterName
     {
         public static string project_type = "project_type";
         public static string selected_project = "selected_project";
         public static string db_connection_string = "db_connection_string";
-    }
+    } 
+    #endregion
 
     #region Helper
     internal class Helper
