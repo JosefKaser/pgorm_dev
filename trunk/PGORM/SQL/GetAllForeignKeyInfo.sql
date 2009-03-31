b843eb13d8788b81fd9427c9c00ecc9ace4ff25d
gevikb <gevikb@96a1c612-1d74-11de-a108-cd2f117ce590>
2009-03-31 22:36:19 +0000

    fix copyright headers SQL files

--- trunk/PGORM/SQL/GetAllForeignKeyInfo.sql
+++ trunk/PGORM/SQL/GetAllForeignKeyInfo.sql
@@ -1,4 +1,19 @@
-﻿select 
+/*-------------------------------------------------------------------------
+ * GetAllForeignKeyInfo.sql
+ *
+ * This file is part of the PGORM project.
+ * http://pgorm.googlecode.com/
+ *
+ * Copyright (c) 2002-2009, TrueSoftware B.V.
+ *
+ * IDENTIFICATION
+ * 
+ *  $Id$
+ * 	$HeadURL$
+ * 	
+ *-------------------------------------------------------------------------
+ */
+select 
 	cn.conname as constraint_name,
 	f.relname as local_table_name,
 	p.relname as foreign_table_name,
