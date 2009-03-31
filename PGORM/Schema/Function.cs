22bf8fb780c130119dfcc79ec7309d957c264082
gevikb <gevikb@96a1c612-1d74-11de-a108-cd2f117ce590>
2009-03-31 09:01:18 +0000

    -- ADD customized DatabaseSchema reader for handling late loading. -- ADD added additional properties to show in the wizard.

--- PGORM/Schema/Function.cs
+++ PGORM/Schema/Function.cs
@@ -12,6 +12,7 @@ namespace PGORM
         public string ReturnTypeType { get; set; }
         public string ReturnTypeRecordSet {get;set;}
         public string FactoryName { get; set; }
+        public string DB_ReturnType { get; set; }
         public List<Column> Parameters = new List<Column>();
         public bool ReturnsSet { get; set; }
 
@@ -20,6 +21,18 @@ namespace PGORM
             string n = FunctionName;
             foreach (Column col in Parameters)
                 n += col.ColumnName + " " + col.CLR_Type.ToString();
+            return n;
+        }
+
+        public string ToStringParamTypes()
+        {
+            string n = "";
+            foreach (Column col in Parameters)
+                n += col.DB_Type + ", ";
+            if (n == "")
+                n = "void";
+            else
+                n = n.Substring(0, n.Length - 2);
             return n;
         }
     }
