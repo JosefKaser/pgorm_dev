10239ff17ff827221b2d0523e4a88b84e8dd5f0b
gevikb <gevikb@96a1c612-1d74-11de-a108-cd2f117ce590>
2009-05-08 11:21:55 +0000

    - Cleanup before renaming namespaces

--- trunk/PostgreSQL/SQL/GetAllEntityColumns.sql
+++
@@ -1,6 +0,0 @@
-﻿
-select
-		t.constraint_type,c.*
-from
-		information_schema.constraint_column_usage c
-		inner join information_schema.table_constraints t on c.constraint_name = t.constraint_name
