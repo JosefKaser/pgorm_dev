3745a7e4c5b36d41b03128bd377a56e009fe216c
gevikb <gevikb@96a1c612-1d74-11de-a108-cd2f117ce590>
2009-06-11 21:34:22 +0000

    --FIX missing "AS" is 8.3 schema reader

--- trunk/DevHelper/Program.cs
+++ trunk/DevHelper/Program.cs
@@ -16,6 +16,7 @@ namespace DevHelper
         {
             //string constr = "server=localhost;database=PGORM_TEST;username=postgres;password=postgres";
             //DataAccess.InitializeDatabase(constr); PGORM.PostgreSQL.CodeHelper.create_helper();
+            //return;
 
             PGORM.CodeBuilder.Project project = new PGORM.CodeBuilder.Project();
             project.DatabaseConnectionInfo.Server = "localhost";
@@ -24,20 +25,11 @@ namespace DevHelper
             project.DatabaseConnectionInfo.Password = "postgres";
             project.OutputFolder = AppDomain.CurrentDomain.BaseDirectory + @"\Output";
             project.BuildInDebugMode = true;
-
-            //project.Tables.Add("public.test1");
-            //project.Tables.Add("public.test2");
-            //project.Tables.Add("public.test3");
-            //project.Tables.Add("public.enum_table");
-            //project.Tables.Add("public.building");
-            //project.Tables.Add("public.building_complex");
-            //project.Tables.Add("public.udt_single_test");
-            //project.Tables.Add("public.udt_array_test");
-            project.Tables.Add("public.udt_multi_array_test");
-
             project.SetDefaultsByDatabaseName();
             project.AssemblyName = @"test1.dll";
             PGORM.CodeBuilder.ProjectBuilder projectBuilder = new PGORM.CodeBuilder.ProjectBuilder(project);
+            projectBuilder.SelectAllObjects();
+
             projectBuilder.OnBuildStep += new PGORM.CodeBuilder.ProjectBuilderEventHandler(projectBuilder_OnBuildStep);
             projectBuilder.Build();
             Console.WriteLine("Done.");
